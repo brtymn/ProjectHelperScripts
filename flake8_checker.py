4a5b66f72d614329f0f9822adf2a0fa2c03c166c
Tomyzon1728 <ajayitomisin1728@gmail.com>
2020-10-02 13:19:36 +0100

    Edited code to fit to PEP8 guidelines as well as grammatical errors

--- flake8_checker.py
+++ flake8_checker.py
@@ -1,25 +1,31 @@
-import os ## The library import that allows the user to execute shell commands with a python script.
-from pathlib import Path ## The library import that allows the user to find file path.
+# This library  allows the user to execute shell commands with a python script.
+import os
+# This library  allows the user to find file path.
+from pathlib import Path 
 
+# User inputs the name of a file inside the target folder.
+project_file_name = input("Please enter file name: ")
 
-project_file_name = input("Please enter the name of a file in the project folder that you want to create 'requirements.txt' for:    ") ## User inputs the name of a file inside the target folder.
-project_file_directory = input("Please enter the directory that may contain the folder:    ") ## User inputs the the path of the file that they wrote for the first input.
+# User inputs the the path of the file that they wrote for the first input.
+project_file_directory = input("Please enter the directory: ") 
 
-
-## This function looks for and finds the desired file. You can specify a parent directory for the fundtion to look for, however if you have no idea where a file is; this functio will find it for you, just slower. If you have no idea where a file is, just type "/".
+"""
+This function looks for and finds the desired file. You can specify a parent directory for the function to look for, however if you have no idea where a file is; this function will find it for you, just slower. If you have no idea where a file is, just type "/".
+"""
 def find_the_project(file_name, directory_name):
-    files_found = []
+    files_found = [ ]
     for path, subdirs, files in os.walk(directory_name):
         for name in files:
             if(file_name == name):
                 file_path = os.path.join(path, name)
                 files_found.append(file_path)
+# Returns the path.
+    return files_found[0] 
 
-    return files_found[0] ## Return the path.
+# Inıtializes the path of the file.
+project_file_path = Path(find_the_project(project_file_name, project_file_directory))
 
-
-project_file_path = Path(find_the_project(project_file_name, project_file_directory)) ## Inıtialize the path of the file.
-
-
-get_shell_statement = "flake8 " + str(project_file_path) ## Initialize the string that will be executed in the shell.
-os.system(get_shell_statement)  ## Execute the predefined string in the shell.
+# Initializes the string that will be executed in the shell.
+get_shell_statement = "flake8 " + str(project_file_path)
+# Executes the predefined string in the shell.
+os.system(get_shell_statement)
